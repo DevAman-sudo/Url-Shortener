6580e44116406db43c6cc00d613674b7190cfd17
DevAmam-sudo <devaman.net@gmail.com>
2021-06-24 20:19:17 +0545

    database connection sucess

--- src/server.js
+++ src/server.js
@@ -11,28 +11,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // file paths ...
-const staticPath = path.join(__dirname , '../public/');
-const viewsPath = path.join(__dirname , '../templates/views');
+const staticPath = path.join(__dirname, '../public/');
+const viewsPath = path.join(__dirname, '../templates/views');
 
 // app middlewares ...
-app.set('view engine' , 'hbs');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static( staticPath ));
+app.use(express.static(staticPath));
 app.use(Routes);
 
 // mongodb connection ...
-mongoose.connect('' , {
-    useNewUrlParser: true
-}).then( () => {
-    console.log('database connected');
-}).catch( (error) => {
-    console.log(`error occured while database connection => ${error}`);
-});
+mongoose.connect(`mongodb+srv://DevAman:${process.env.PASS}@cluster0.tlrz1.mongodb.net/devlink?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(() => {
+        console.log('database connected');
+    }).catch((error) => {
+        console.log(`error occured while database connection => ${error}`);
+    });
 
 // listening to app on port 5000 ...
-app.listen(port , (error) => {
+app.listen(port, (error) => {
     if (error) {
         console.log(`error occured while listening to app => ${error}`);
     } else {
