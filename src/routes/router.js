ebbc1e161e634373f4cb3ebec0163ab2989abab5
DevAmam-sudo <devaman.net@gmail.com>
2021-06-26 12:39:50 +0545

    npm package update

--- src/routes/router.js
+++ src/routes/router.js
@@ -9,4 +9,16 @@ router.get('/' , async (req , res) => {
     res.status(200).render('index');
 });
 
+// index shortner post route ...
+router.post('/shortner' , async (req , res) => {
+    try {
+        
+        console.log(req.body.short);
+        res.redirect('/');
+        
+    } catch (error) {
+        res.status(500).send(`shortner route error => ${error}`);
+    }
+});
+
 module.exports = router;
