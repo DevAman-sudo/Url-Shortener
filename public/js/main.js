0721821befe42f2beed54dddb037ed68026ce50d
DevAmam-sudo <devaman.net@gmail.com>
2021-06-26 12:10:32 +0545

    client side code update

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// DOM elements ...
+const indexSignup1 = document.getElementById('index_signup1');
+const indexSignup2 = document.getElementById('index_signup2');
+
+// index signup button location redirect ...
+indexSignup1.addEventListener('click' , () => {
+    window.location = '/signup';
+});
+indexSignup2.addEventListener('click' , () => {
+    window.location = '/signup';
+});
