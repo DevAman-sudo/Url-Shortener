17db9e7c912354618593a0de78082e249d1c9ba4
DevAman-sudo <devaman.net@gmail.com>
2021-07-05 12:02:47 +0545

    root page started to function

--- src/models/user.js
+++ src/models/user.js
@@ -7,100 +7,100 @@ const shortid = require('shortid');
 
 const userSchema = new mongoose.Schema({
 
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        minimun: 2,
-        maximun: 32
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        lowercase: true,
-        maximun: 255,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email is Invalid");
-            }
-        }
-    },
-    pass: {
-        type: String,
-        required: true,
-        trim: true,
-        minimun: 8,
-        maximun: 255
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minimun: 8,
-        maximun: 255
-    },
-    confirm_password: {
-        type: String,
-        required: true,
-        trim: true,
-        minimun: 8,
-        maximun: 25
-    },
-    date: {
-        type: Date,
-        default: Date.now
-        },
-        tokens: [{
-            token: {
-                type: String,
-                required: true
-            }
-        }],
-        url: {
-            fullurl: {
-                type: String,
-            },
-            shorturl: {
-                type: String,
-                required: true,
-                default: shortid.generate
-                }
-            }
+	username: {
+		type: String,
+		required: true,
+		trim: true,
+		minimun: 2,
+		maximun: 32
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: true,
+		lowercase: true,
+		maximun: 255,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("Email is Invalid");
+			}
+		}
+	},
+	pass: {
+		type: String,
+		required: true,
+		trim: true,
+		minimun: 8,
+		maximun: 255
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minimun: 8,
+		maximun: 255
+	},
+	confirm_password: {
+		type: String,
+		required: true,
+		trim: true,
+		minimun: 8,
+		maximun: 25
+	},
+	date: {
+		type: Date,
+		default: Date.now
+		},
+		tokens: [{
+			token: {
+				type: String,
+				required: true
+			}
+		}],
+		urls: [{
+			fullurl: {
+				type: String,
+			},
+			shorturl: {
+				type: String,
+				required: true,
+				default: shortid.generate
+				}
+			}]
 
-        });
+		});
 
-    // generating auth token middleware
-    userSchema.methods.generateAuthToken = async function () {
-        try {
+	// generating auth token middleware
+	userSchema.methods.generateAuthToken = async function () {
+		try {
 
-            const token = jwt.sign({
-                _id: this._id.toString()
-            },
-                process.env.JWT_KEY);
-            this.tokens = this.tokens.concat({
-                token: token
-            });
-            await this.save();
-            return token;
+			const token = jwt.sign({
+				_id: this._id.toString()
+			},
+				process.env.JWT_KEY);
+			this.tokens = this.tokens.concat({
+				token: token
+			});
+			await this.save();
+			return token;
 
-        } catch (error) {
-            res.send(error);
-            console.log(`Error occured while generating jwt => ${error}`);
-        }
-    };
+		} catch (error) {
+			res.send(error);
+			console.log(`Error occured while generating jwt => ${error}`);
+		}
+	};
 
-    // hashing password middleware
-    userSchema.pre("save", async function() {
+	// hashing password middleware
+	userSchema.pre("save", async function() {
 
-        if (this.isModified("password")) {
-            this.password = await bcrypt.hash(this.password, 10);
-            this.confirm_password = await bcrypt.hash(this.confirm_password, 10);
-        }
+		if (this.isModified("password")) {
+			this.password = await bcrypt.hash(this.password, 10);
+			this.confirm_password = await bcrypt.hash(this.confirm_password, 10);
+		}
 
-    });
+	});
 
-    const User = new mongoose.model("User", userSchema);
+	const User = new mongoose.model("User", userSchema);
 
-    module.exports = User;
+	module.exports = User;
--- src/routes/router.js
+++ src/routes/router.js
@@ -12,185 +12,200 @@ router.set('views', path.join(__dirname, '../../templates/views'));
 // index route ...
 router.get('/', async (req, res) => {
 
-    const shortUrls = await Shortner.find();
-    res.status(200).render('index', {
-        shortUrls: shortUrls,
-    });
+	const shortUrls = await Shortner.find();
+	res.status(200).render('index', {
+		shortUrls: shortUrls,
+	});
 });
 
 // index shortner post route ...
 router.post('/shortner', async (req, res) => {
-    try {
+	try {
 
-        await Shortner.create({
-            fullurl: req.body.short
-        });
-        res.redirect('/');
+		await Shortner.create({
+			fullurl: req.body.short
+		});
+		res.redirect('/');
 
-    } catch (error) {
-        res.status(500).send(`shortner route error => ${error}`);
-    }
+	} catch (error) {
+		res.status(500).send(`shortner route error => ${error}`);
+	}
 });
 
 // signup route ...
-router.get('/signup' , (req , res) => {
-    res.render('signup');
+router.get('/signup', (req, res) => {
+	res.render('signup');
 });
 
 // signup post routes ...
 router.post('/signup', (req, res) => {
 
-    const createDocument = async () => {
-        try {
-            const Password = req.body.password;
-            const Confirm_password = req.body.confirm_password;
+	const createDocument = async () => {
+		try {
+			const Password = req.body.password;
+			const Confirm_password = req.body.confirm_password;
 
-            if (Password == Confirm_password) {
-                const registerUser = new User({
-                    username: req.body.username,
-                    email: req.body.email,
-                    pass: req.body.password,
-                    password: Password,
-                    confirm_password: Confirm_password
-                });
+			if (Password == Confirm_password) {
+				const registerUser = new User({
+					username: req.body.username,
+					email: req.body.email,
+					pass: req.body.password,
+					password: Password,
+					confirm_password: Confirm_password
+				});
 
-                const registered = await registerUser.save();
+				const registered = await registerUser.save();
 
-                res.status(201).redirect('/user/login');
-            } else {
-                res.status(201).send('password didn`t matched');
-            }
+				res.status(201).redirect('/user/login');
+			} else {
+				res.status(201).send('password didn`t matched');
+			}
 
-        } catch(error) {
-            res.status(400).send(error);
-        }
-    };
-    createDocument();
+		} catch(error) {
+			res.status(400).send(error);
+		}
+	};
+	createDocument();
 
 });
 
 
 // login route ...
-router.get('/user/login' , (req , res) => {
-    res.render('login');
+router.get('/user/login', (req, res) => {
+	res.render('login');
 });
 
 // login post routes ...
 router.post('/login', async (req, res) => {
-    try {
+	try {
 
-        const email = req.body.email;
-        const password = req.body.password;
+		const email = req.body.email;
+		const password = req.body.password;
 
-        const userData = await User.findOne({
-            email: email
-        });
+		const userData = await User.findOne({
+			email: email
+		});
 
-        // comparing hashed password with user password
-        const isMatch = await bcrypt.compare(password, userData.password);
+		// comparing hashed password with user password
+		const isMatch = await bcrypt.compare(password, userData.password);
 
-        // JWT auth tokens
-        const token = await userData.generateAuthToken();
+		// JWT auth tokens
+		const token = await userData.generateAuthToken();
 
-        // storing user cookie
-        res.cookie("jwt", token, {
-            expires: new Date(Date.now() + 600000),
-            httpOnly: true,
-            // secure: true
-        });
+		// storing user cookie
+		res.cookie("jwt", token, {
+			expires: new Date(Date.now() + 600000),
+			httpOnly: true,
+			// secure: true
+		});
 
-        if (isMatch) {
-            res.status(201).redirect(`/user/${userData._id}`);
-        } else {
-            res.send('password didnt matched');
-        }
+		if (isMatch) {
+			res.status(201).redirect(`/user/${userData._id}`);
+		} else {
+			res.send('password didnt matched');
+		}
 
-    } catch (error) {
-        res.status(400).send('invalid login details');
-        console.log(`error occured => ${error}`);
-    }
+	} catch (error) {
+		res.status(400).send('invalid login details');
+		console.log(`error occured => ${error}`);
+	}
 });
 
 // root user auth route ...
-router.get('/user/:id' , auth , async (req , res) => {
-    try {
+router.get('/user/:id', auth, async (req, res) => {
+	try {
 
-        const id = await User.findOne({ _id: req.params.id });
-        console.log(id.username);
+		const doc = await User.findOne({ _id: req.params.id });
 
-        res.status(200).render('root');
-        
-    } catch (error) {
-        res.status(500).send(error);
-    }
+		res.status(200).render('root' , {
+			doc: doc.urls
+		});
+
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 // root page post route ...
-router.post('/user/root' , auth , async (req , res) => {
-    try {
-        
-        // console.log(req.params.id);
-        console.log(req.body.short);
-        res.redirect('/user/login');
-        
-    } catch (error) {
-        res.status(500).send(error);
-    }
+router.post('/user/root', auth, async (req, res) => {
+	try {
+		
+		const doc = await User.findOne({ email: 'devaman.net@gmail.com' });
+
+		User.updateOne(
+			{
+				email: 'devaman.net@gmail.com'
+			},
+			{
+				$addToSet: {
+					urls: [{
+						fullurl: req.body.short
+					}]
+				}
+			}, {
+				new: true, upsert: true
+			}).exec();
+
+		res.redirect(`/user/${doc._id}}`);
+
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 // logout routes ...
-router.get('/logout' , auth , async (req , res) => {
-    try {
-        
-        // removing cookie from database
-        req.user.tokens = req.user.tokens.filter( (currentToken) => {
-            return currentToken.token !== req.token;
-        });
-        
-        // removing cookie from client machine
-        res.clearCookie("jwt");
-        await req.user.save();
-        res.redirect('/user/login');
-        
-    } catch (error) {
-        res.status(500).send(`logout route error => ${error}`);
-    }
+router.get('/logout', auth, async (req, res) => {
+	try {
+
+		// removing cookie from database
+		req.user.tokens = req.user.tokens.filter((currentToken) => {
+			return currentToken.token !== req.token;
+		});
+
+		// removing cookie from client machine
+		res.clearCookie("jwt");
+		await req.user.save();
+		res.redirect('/user/login');
+
+	} catch (error) {
+		res.status(500).send(`logout route error => ${error}`);
+	}
 });
 
 // logout routes ...
-router.get('/logoutall' , auth , async (req , res) => {
-    try {
-        
-        // logout from all devices
-        req.user.tokens = [];
-        
-        // removing cookie from client machine
-        res.clearCookie("jwt");
-        await req.user.save();
-        res.redirect('/user/login');
-        
-    } catch (error) {
-        res.status(500).send(`logout route error => ${error}`);
-    }
+router.get('/logoutall', auth, async (req, res) => {
+	try {
+
+		// logout from all devices
+		req.user.tokens = [];
+
+		// removing cookie from client machine
+		res.clearCookie("jwt");
+		await req.user.save();
+		res.redirect('/user/login');
+
+	} catch (error) {
+		res.status(500).send(`logout route error => ${error}`);
+	}
 });
 
 // 404 error route ...
 router.get('*', (req, res) => {
-    res.render('error');
+	res.render('error');
 });
 
 // short id index route ...
 router.get('/:id', async (req, res) => {
-    try {
+	try {
 
-        const shortId = await Shortner.findOne({
-            shorturl: req.params.id
-        });
-        res.redirect(Shortner.fullurl);
+		const shortId = await Shortner.findOne({
+			shorturl: req.params.id
+		});
+		res.redirect(Shortner.fullurl);
 
-    } catch (error) {
-        res.status(500).send(error);
-    }
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 module.exports = router;
